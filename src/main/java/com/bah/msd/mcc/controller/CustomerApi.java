8eee8337af13611d727e08dff3f761578ca71d41
Kendall Wiggins <kendall@tarpaw.com>
2019-11-07 15:05:24 -0500

    All working CRUD ops for API (finding by ID)

--- src/main/java/com/bah/msd/mcc/controller/CustomerApi.java
+++ src/main/java/com/bah/msd/mcc/controller/CustomerApi.java
@@ -1,17 +1,17 @@
 package com.bah.msd.mcc.controller;
 
 import java.net.URI;
-//import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-//import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import org.springframework.web.util.UriComponentsBuilder;
@@ -30,11 +30,11 @@ public class CustomerApi {
 
 	// get a list of all customers
 	// URL: http://localhost:8080/customers/all
-	
-	@GetMapping("/all") // need to add produces = "JSON"
+
+	@GetMapping("/all") // need to add produces = "JSON"?
 	public Iterable<Customer> getAllCustomers() {
 		System.out.println("This is a test to see if get all worked!");
-		//System.out.println(this.customerRepo.findAll().toString());
+		// System.out.println(this.customerRepo.findAll().toString());
 		return customerRepo.findAll();
 	}
 
@@ -47,27 +47,70 @@ public class CustomerApi {
 	 * System.out.println("This is a test to see if get by name worked!"); return
 	 * this.customerRepo.findByName(customerName); }
 	 */
-	
+
 	// get a customer's info by their id
 	// URL: http://localhost:8080/customers/{id number}
-	
-	
+
 	@GetMapping("/{customerId}")
 	public Optional<Customer> findById(@PathVariable("customerId") long id) {
 		System.out.println("This is a test to see if get by id worked!");
-		return customerRepo.findById(id);	// this is built-in in CRUD repository; do not need to add to CustomerRepository.java
+		return customerRepo.findById(id); // this is built-in in CRUD repository; do not need to add to
+											// CustomerRepository.java
 	}
-	
+
 	// add and save a new customer
 	@PostMapping
 	public ResponseEntity<?> addCustomer(@RequestBody Customer newCustomer, UriComponentsBuilder uri) {
-		if (newCustomer.getId() != 0 || newCustomer.getName() == null || newCustomer.getEmail() == null) {
+		if (newCustomer.getId() != 0 
+				|| newCustomer.getName() == null 
+				|| newCustomer.getEmail() == null) {
 			return ResponseEntity.badRequest().build();
 		}
+		
 		newCustomer = customerRepo.save(newCustomer); // save is part of CRUD repository
 		URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
 				.buildAndExpand(newCustomer.getId()).toUri();
 		ResponseEntity<?> response = ResponseEntity.created(location).build();
 		return response;
 	}
+
+	// Update existing customer
+	@PutMapping("/{customerId}")
+	public ResponseEntity<?> putCustomer(@RequestBody Customer newCustomer,
+			@PathVariable("customerId") long customerId) {
+		if (newCustomer.getId() != customerId || newCustomer.getName() == null || newCustomer.getEmail() == null) {
+			return ResponseEntity.badRequest().build();
+		}
+		newCustomer = customerRepo.save(newCustomer); // save is part of CRUD repository
+		return ResponseEntity.ok().build();
+	}
+
+	// Delete an existing customer
+	@DeleteMapping("/{customerId}")
+	public ResponseEntity<?> deleteCustomer(@RequestBody Customer delCustomer, @PathVariable Long customerId) {
+		if (delCustomer.getId() != customerId 
+				|| delCustomer.getName() == null 
+				|| delCustomer.getEmail() == null) {
+			return ResponseEntity.badRequest().build();
+		}
+		customerRepo.deleteById(customerId);
+		return ResponseEntity.ok().build();
+	}
+
+//	@DeleteMapping(value = "/{customerId}")
+//	public ResponseEntity<?> deleteCustomer(@PathVariable Long customerId) {
+//		if (priorCustomer.getId() != customerId || priorCustomer.getName() == null || priorCustomer.getEmail() == null) {			
+//			return ResponseEntity.badRequest().build();		}
+//		}
+//		
+//		Customer customerToRemove = customerRepo.deleteById(customerId);
+//	       if (!customerToRemove) {
+////	            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+////	        }
+////
+////	        return new ResponseEntity<>(customerId, HttpStatus.OK);
+////	    }
+////		//priorCustomer = customerRepo.deleteById(priorCustomer.getId()); // save is part of CRUD repository
+////		return ResponseEntity.ok().build();
+////}
 }
